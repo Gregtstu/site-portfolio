8e06e13e97e4d52a609bf0d47d78fe1859b16565
Gregtstu <boltnev.grisha@yandex.ru>
2022-08-03 08:14:44 +0300

    добавил мобильную менюшку

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+const nav = document.querySelector('.header__navigotion');
+const navUl = document.querySelector('.navigotion__list');
+const navBtn = document.querySelector('#btn');
+
+navBtn.addEventListener('click', ()=> {
+    if(nav.classList.toggle('open')){
+        navBtn.src = "./img/icons/nav-close.svg";
+    } else{
+        navBtn.src = "./img/icons/nav-open.svg";
+    }
+})
